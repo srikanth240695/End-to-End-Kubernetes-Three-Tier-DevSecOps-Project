b3a7ce909ea02f5d75301fb232b9fb403358e3b5
srikanth240695 <77737462+srikanth240695@users.noreply.github.com>
2026-05-14 12:01:17 +0530

    Delete Jenkis-Server directory

--- Jenkis-Server/modules/sg/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "jenkins_security_group_id" {
-  description = "The ID of the security group to be assigned to the EC2 instance"
-  value       = aws_security_group.jenkins_sg.id
-}
-
-output "alb_security_group_id" {
-  description = "The ID of the security group to be assigned to the ALB"
-  value       = aws_security_group.alb_sg.id
-}
